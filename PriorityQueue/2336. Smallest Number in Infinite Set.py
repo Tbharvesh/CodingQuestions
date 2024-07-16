8d652c9684494000bfe643012ed1a4c008c6c5e5
Tbharvesh <tanisha.bharvesh@gmail.com>
2024-07-16 13:10:55 +0530

    up

--- PriorityQueue/2336. Smallest Number in Infinite Set.py
+++ PriorityQueue/2336. Smallest Number in Infinite Set.py
@@ -1,5 +1,32 @@
-class SmallestInfiniteSet:
+# Intuition : 
 
+# Method 1 PriorityQ
+
+import heapq
+
+class SmallestInfiniteSet: # type: ignore
+    def __init__(self):
+        self.heap = []
+        self.set = set()
+        self.smallest = 1
+
+    def popSmallest(self) -> int:
+        if self.heap:
+            smallest = heapq.heappop(self.heap)
+            self.set.remove(smallest)
+            return smallest
+        else:
+            result = self.smallest
+            self.smallest += 1
+            return result
+
+    def addBack(self, num: int) -> None:
+        if num < self.smallest and num not in self.set:
+            heapq.heappush(self.heap, num)
+            self.set.add(num)
+            
+# Method2 _ sets
+class SmallestInfiniteSet:
     def __init__(self):
         self.set = set()
         self.cur = 1 # cur stores the current smallest elem
