e6406c098c6bb69483b19e4780dc87a6cd37f309
Tbharvesh <tanisha.bharvesh@gmail.com>
2024-08-09 13:47:40 +0530

    bs5.1

--- BinarySearch/1283. Find the Smallest Divisor Given a Threshold.py
+++ BinarySearch/1283. Find the Smallest Divisor Given a Threshold.py
@@ -1,5 +1,7 @@
 import math
 from typing import List
+# As the deno. is getting inc --> ans is dec 
+
 class Solution:
     def divide(self,nums,mid):
         tot = 0
@@ -8,10 +10,10 @@ class Solution:
         return tot
     def smallestDivisor(self, nums: List[int], threshold: int) -> int:
         l , h = 1, max(nums)
-       
+        # Here we are not working exactly on the arr ,but applying Binary serach on a range which lies between 1 and max elem
         while(l <= h):
             m = (l + h)//2
-            tempAns =self.divide(nums,m)
+            tempAns =self.divide(nums,m) #If tempAns > threshold , we need to inc the denominator so that ans will dec
             if tempAns > threshold :
                 l = m + 1
             else:
